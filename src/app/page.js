670f3c1c319badd140c6cdb0ffb38eb9d61878f3
valenpe <valentinaperez230701@gmail.com>
2023-11-20 15:37:05 -0300

    primer commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#191919]">
+      <section>
+        <h1 className="text-white">Hola, mi nombre es Valen</h1>
+      </section>
+    </main>
+  );
+}
