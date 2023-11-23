cd1661885ebd028716d7bbf643156ab35b5d7ef8
valenpe <valentinaperez230701@gmail.com>
2023-11-23 03:13:02 -0300

    mejoras, hay que seguir viendo el tema de los botones

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <section className="flex min-h-screen flex-col bg-[#191919] container mx-auto px-12 py-4">
-      <HeroSection />
+    <section className="flex min-h-screen flex-col bg-[#191919] ">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </section>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,37 +2,39 @@ import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7  lg:col-span-6  place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 sm:text-5xl text-4xl lg:text-6xl font-extrabold">
             Hola, soy Valen!
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl relative">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
           </p>
         </div>
         <div className="">
-          <button className="px-6 py-3 rounded-full  bg-white hover:bg-slate-200 text-black mr-4">
+          <button className="px-6 py-3 w-full sm:w-fit rounded-full  bg-white hover:bg-slate-200 text-black mr-4 ">
             Hire me
           </button>
-          <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+          <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
             Download CV
           </button>
         </div>
-        <div className=" col-span-5  place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#202020] w-[250px] h-[250px] lg:h-[400px] lg:w-[400px] relative overflow-hidden">
-            <Image
-              src="/mifoto.png"
-              width={300}
-              height={300}
-              alt="mifoto"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            />
+        <div>
+          <div className=" col-span-5  place-self-center mt-4 lg:mt-0">
+            <div className="rounded-full bg-[#202020] w-[250px] h-[250px] lg:h-[400px] lg:w-[400px] relative overflow-hidden">
+              <Image
+                src="/mifoto.png"
+                width={300}
+                height={300}
+                alt="mifoto"
+                className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
