ae9c45e4ec3192ac8f61754a65b1a61d36da245e
valenpe <valentinaperez230701@gmail.com>
2023-12-05 01:46:26 -0300

    culminada seccion projets

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -6,6 +6,8 @@ const projectsData = [
     title: "React Project Rick and Morty",
     description: "Detail of the characters of the series",
     image: "/projects/pr1.jpeg",
+    gitUrl: "https://github.com/Vvalentinaperez/RickAndMorty.git",
+    previewUrl: "/",
   },
   {
     id: 2,
@@ -13,12 +15,16 @@ const projectsData = [
     description:
       "Application with information about Pokemon and the possibility of creating new ones",
     image: "/projects/pr2.jpeg",
+    gitUrl: "https://github.com/Vvalentinaperez/PI-Pokemon.git",
+    previewUrl: "/",
   },
   {
     id: 3,
     title: "E-commerce Black Group Design",
     description: "page for purchasing furniture and hiring decoration services",
     image: "/projects/pr3.jpeg",
+    gitUrl: "https://github.com/bgdesign23",
+    previewUrl: "/",
   },
 ];
 
@@ -35,6 +41,8 @@ const ProjectsSection = () => {
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
